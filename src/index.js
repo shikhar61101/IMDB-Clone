caff14d02e3ed6d57fe20af852db739cfde3b18f
Shikhar <shikhar61101@gmail.com>
2024-05-23 11:36:07 +0530

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+//import { createRoot } from 'react-dom/client';
+ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Importing necessary components from react-router-dom
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import App from './App';
+import Header from './components/header/Header';
+import Home from './components/pages/home/Home';
+import Movie from './components/pages/movieDetail/Movie';
+import { MovieList } from './components/movieList/MovieList';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+const CustomRoutes = () => (
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/*" element={<h1>Error</h1>} />
+    <Route path="/movie/:id" element={<Movie />} />
+    <Route path="/movies/:type" element={<MovieList />} />
+  </Routes>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const container = document.getElementById('app');
+// const root = createRoot(container); // createRoot(container!) if you use TypeScript
+// root.render(<App />);
+
+ReactDOM.render(
+    <Router>
+      <Header />
+      <CustomRoutes /> {/* Render your custom routes */}
+    </Router>,
+    document.getElementById('root')
+  );
